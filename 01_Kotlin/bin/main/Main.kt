1cbf1c356f8da974b192b63e1cdcb89f73a60358
GiulianDavid <giulian.albuja@epn.edu.ec>
2023-11-08 16:05:03 -0500

    Commit dos

---
+++ 01_Kotlin/bin/main/Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    println("Hello Worldddd!")
+
+    // Try adding program arguments at Run/Debug configuration
+    println("Program arguments: ${args.joinToString()}")
+}
